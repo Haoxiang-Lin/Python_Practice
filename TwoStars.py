f383ab962c310206c3a3601697ae3f6f79cb3a15
sanniu <972852757apple@gmail.com>
2014-03-29 01:25:46 +0800

    Using matplotlib this time

--- TwoStars.py
+++ TwoStars.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
-from reportlab.graphics.shapes import *
-from reportlab.graphics.charts.lineplots import LinePlot
-from reportlab.graphics.charts.textlabels import Label
-from reportlab.graphics import renderPDF
+import matplotlib.pyplot as plt
 
 def OpenData(file_name):
     '''
@@ -12,9 +9,9 @@ def OpenData(file_name):
     '''
     log = open(file_name, 'r')
     data = []
-    #去掉第一行
+    #去文件头
     log.readline()
-    #读取数据并转换为
+    #读取数据并转换为数字
     for line in log:
         data.append( [float(str) for str in list(line.split())] )
     log.close()
@@ -32,59 +29,33 @@ def OpenData(file_name):
     zcmp  = [row[10] for row in data]
     return (t, v1r, v2r, Mbol, dS, y1p, z1p, y2p, z2p, ycmp, zcmp)
 
-def Draw(var_h, var_v, title_name, var_h_str, var_v_str):
+def Draw(var_h, var_v):
     '''
     画图
     '''
-    lp = LinePlot()
-    #原点位置
-    lp.x = 50
-    lp.y = 50
-    #坐标轴长宽
-    lp.height = 125
-    lp.width  = 300
-    #使用数据
-    lp.data = []
-    for i in range(len(var_h)) :
-        lp.data.append(zip(var_h[i], var_v[i]))
+    font = {'family' : 'serif',
+        'color'  : 'darkred',
+        'weight' : 'normal',
+        'size'   : 16,
+        }
 
-    drawing = Drawing(400, 200)
-    drawing.add(lp)
+    plt.plot(var_h[70], var_v[70], 'r', label=r'$ 70^{\degree} $')
+    plt.plot(var_h[75], var_v[75], 'c', label=r'$ 75^{\degree} $')
+    plt.plot(var_h[80], var_v[80], 'm', label=r'$ 80^{\degree} $')
+    plt.plot(var_h[85], var_v[85], 'y', label=r'$ 85^{\degree} $')
+    plt.plot(var_h[90], var_v[90], 'b', label=r'$ 90^{\degree} $')
+    plt.title('Light Curve', fontdict=font)
+    plt.xlabel('Period', fontdict=font)
+    plt.ylabel(r'Magnitude', fontdict=font)
+    #plt.ylabel(r'Luminosity\ $ (L_{\bigodot}) $', fontdict=font)
+    plt.subplots_adjust(left=0.15)
+    #plt.axis([0.85, 1, -2.6, -1.4])
+    #plt.axis([0.28, 0.45, -2.6, -1.4])
+    plt.axis([0, 1, -2.6, -1.4])
+    plt.legend(loc='center left')
+    plt.gca().invert_yaxis()
+    plt.show()
 
-    #曲线颜色和名字
-    lp.lines[0].strokeColor = colors.blue
-    lp.lines[1].strokeColor = colors.red
-    lp.lines[2].strokeColor = colors.green
-    lp.lines[3].strokeColor = colors.black
-    #drawing.add(String(200, 180, 'Curve1', fontSize=14, fillColor=colors.red))
-
-    #标题
-    title = Label()
-    title._text = title_name
-    title.fontSize    = 14
-    title.x           = 200
-    title.y           = 180
-    drawing.add(title)
-
-    #x轴标签
-    Xlabel = Label()
-    Xlabel._text = var_h_str
-    Xlabel.fontSize   = 10
-    Xlabel.x          = 380
-    Xlabel.y          = 40
-    Xlabel.textAnchor ='middle'
-    drawing.add(Xlabel)
-
-    #y轴标签
-    Ylabel = Label()
-    Ylabel._text = var_v_str
-    Ylabel.fontSize   = 10
-    Ylabel.x          = 30
-    Ylabel.y          = 180
-    Ylabel.textAnchor ='middle'
-    drawing.add(Ylabel)
-
-    renderPDF.drawToFile(drawing, 'LinePlot.pdf')
 
 def main():
     var_list_str = ('t', 'v1r', 'v2r', 'Mbol','dS', 'y1p', 'z1p', 'y2p', 'z2p', 'ycmp', 'zcmp')
@@ -93,25 +64,25 @@ def main():
     print '  *                                                                    *'
     print '  * Hi! This program is designed to plot Curve diagram with data from  *'
     print '  * TwoStars.cpp.                                                      *'
-    print '  * The diagram would be generated as pdf file                         *'
     print '  *                                                                    *'
     print '  **********************************************************************'
-    print '  Now please: '
     var_h_str = 't'
-    var_v_str = 'dS'
+    var_v_str = 'Mbol'
     var_h = []
     var_v = []
+    i = 0
     while True :
-        file_name = raw_input('  Input name of the file from TwoStars.cpp (Input "ok" if done): ')
-        if file_name == 'ok' :
+        file_name = str(i)#raw_input('  Input name of the file from TwoStars.cpp (Input "ok" if done): ')
+        if file_name == '91' :
             break
         #打开文件
         var_list = OpenData(file_name)
         #读取两个变量
         var_h.append(var_list[var_list_str.index(var_h_str)])
         var_v.append(var_list[var_list_str.index(var_v_str)])
+        i += 1
     #画图
-    Draw(var_h, var_v, 'Light Curve', var_h_str, var_v_str)
+    Draw(var_h, var_v)
 
 if __name__ == '__main__':
     main()
