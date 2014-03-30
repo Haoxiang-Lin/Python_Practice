cf0d0d602fbe588c667d20d2728540e2f30acd2c
sanniu <972852757apple@gmail.com>
2014-03-31 01:28:04 +0800

    Add SAWs

--- RWs.py
+++ RWs.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import matplotlib.pyplot as plt
-from random import *
+from random import choice
 
 class RandomWalk:
     def __init__(self):
@@ -41,7 +41,7 @@ class RandomWalk:
             plt.ylabel('y')
             plt.title('Free Random Walk')
             plt.show()
-        self.k = 0     #重置步数
+        self.k = 0
 
     def NRRW(self, N):
         """
@@ -72,10 +72,47 @@ class RandomWalk:
             plt.ylabel('y')
             plt.title('Non Reversal Random Walk')
             plt.show()
-        self.k = 0     #重置步数
+        self.k = 0
 
-if __name__ == '__main__' :
-    rw = RandomWalk()
-    rw.FRW(1000)
-    rw.NRRW(1000)
-    print rw.x, rw.y
+    def SAW(self, N):
+        """
+        自避随机行走
+        """
+        if N != int(N) :
+            print "N must be an integer"
+        else :
+            pre = []
+            while self.k != N :
+                pre.append([self.x, self.y])
+
+                self.v = choice([self.v - 1, self.v, self.v + 1])
+                self.v %= 4
+
+                if self.v == 0 :
+                    self.x += 1
+                elif self.v == 1 :
+                    self.y += 1
+                elif self.v == 2 :
+                    self.x += -1
+                elif self.v == 3 :
+                    self.y += -1
+                self.k += 1
+
+                if [self.x, self.y] in pre :
+                    self.__init__()
+                    self.SAW(N)
+                    exit(0)
+            for i in range(len(pre) - 1):
+                plt.plot([pre[i][0], pre[i+1][0]], [pre[i][1], pre[i+1][1]])
+            plt.xlabel('x')
+            plt.ylabel('y')
+            plt.title('Self-Avoiding Random Walk')
+            plt.show()
+        self.k = 0
+
+
+# if __name__ == '__main__' :
+#     rw = RandomWalk()
+#     rw.FRW(1000)
+#     rw.NRRW(1000)
+#     rw.SAW(50)
